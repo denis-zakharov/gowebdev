f21020c901431caa8a2104e7b2f2dcb932c61f2b
Denis Zakharov <dizaharov@gmail.com>
2021-12-02 23:22:11 +0300

    modd tool: hot reloading

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to my site!</h1>")
+	fmt.Fprintf(w, "<h1>Welcome to my great site!</h1>")
 }
 
 func main() {
