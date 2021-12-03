a3766cb1bab51c74692d93841ff7de3341bc4ce2
Denis Zakharov <dizaharov@gmail.com>
2021-12-03 10:15:25 +0300

    the simplest router example

--- main.go
+++ main.go
@@ -11,13 +11,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch email me at "+
 		"<a href=\"mailto:dizaharov@gmail.com\">dizaharov@gmail.com</a>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		homeHandler(w, r)
+	} else if r.URL.Path == "/contact" {
+		contactHandler(w, r)
+	} else {
+		fmt.Fprintf(w, r.URL.Path)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the web server on 3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
